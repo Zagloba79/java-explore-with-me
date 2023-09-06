80f6fc53851dad71cd4665824c4e6353456cfcf9
Zagloba <an9262778931@yandex.ru>
2023-09-06 12:08:56 +0300

    2nd part

--- ewm-service/src/main/java/ru/practicum/ewm/service/PublicServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/PublicServiceImpl.java
@@ -138,4 +138,5 @@ public class PublicServiceImpl implements PublicService {
                 .orElseThrow(() -> new ObjectNotFoundException("Compilation not found"));
         return CompilationMapper.toCompilationDto(compilation);
     }
+
 }
