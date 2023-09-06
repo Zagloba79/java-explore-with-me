4c02ab8013e04b6479db66cd5a5304a7847a0628
Zagloba <an9262778931@yandex.ru>
2023-09-06 21:07:42 +0300

    2nd part

--- stats-service/server/src/main/java/ru/practicum/repository/StatRepository.java
+++ stats-service/server/src/main/java/ru/practicum/repository/StatRepository.java
@@ -11,29 +11,33 @@ import java.util.List;
 
 @Repository
 public interface StatRepository extends JpaRepository<EndpointHit, Long> {
-    @Query("SELECT new ru.practicum.model.ViewStats(eh.app, eh.uri, COUNT(DISTINCT (eh.ip))) " +
+    @Query("SELECT new ru.practicum.model.ViewStats(eh.app, eh.uri, COUNT(DISTINCT eh.ip)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.uri IN (?1) " +
             "AND eh.timestamp BETWEEN ?2 and ?3 " +
-            "GROUP BY eh.ip, eh.app, eh.uri ")
+            "GROUP BY eh.ip, eh.app, eh.uri " +
+            "ORDER BY COUNT(distinct eh.ip) desc ")
     List<ViewStats> getUniqueHitsListByUris(List<String> uris, LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(eh.app, eh.uri, COUNT(DISTINCT (eh.ip))) " +
+    @Query("SELECT new ru.practicum.model.ViewStats(eh.app, eh.uri, COUNT(DISTINCT eh.ip)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.timestamp BETWEEN ?1 and ?2 " +
-            "GROUP BY eh.ip, eh.app, eh.uri ")
+            "GROUP BY eh.ip, eh.app, eh.uri " +
+            "ORDER BY COUNT(distinct eh.ip) desc ")
     List<ViewStats> getUniqueHitsList(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.model.ViewStats(eh.app, eh.uri, COUNT(*)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.uri IN (?1) " +
             "AND eh.timestamp BETWEEN ?2 and ?3 " +
-            "GROUP BY eh.app, eh.uri ")
+            "GROUP BY eh.app, eh.uri " +
+            "ORDER BY COUNT(eh.ip) desc ")
     List<ViewStats> getHitsListByUris(List<String> uris, LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.model.ViewStats(eh.app, eh.uri, COUNT(*)) " +
             "FROM EndpointHit eh " +
             "WHERE eh.timestamp BETWEEN ?1 and ?2 " +
-            "GROUP BY eh.app, eh.uri ")
+            "GROUP BY eh.app, eh.uri " +
+            "ORDER BY COUNT(eh.ip) desc ")
     List<ViewStats> getHitsList(LocalDateTime start, LocalDateTime end);
 }
